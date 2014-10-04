ea69831243e072d7dadd7e19f08e626cd26d39b1
Vadym Yatsyuk <vadym.yatsyuk@innosabi.com>
2014-10-04 16:51:31 +0200

    removed submodule Alamofire

---
+++ UnserAller/MenuTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MenuTableViewController.swift
+//  UnserAller
+//
+//  Created by Vadym on 04/10/14.
+//  Copyright (c) 2014 Vadym Yatsyuk. All rights reserved.
+//
+
+import Foundation
---
+++ UnserAller/ViewControllers/InitNavigationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InitNavigationViewController.swift
+//  UnserAller
+//
+//  Created by Vadym on 04/10/14.
+//  Copyright (c) 2014 Vadym Yatsyuk. All rights reserved.
+//
+
+import UIKit
+
+class InitNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
